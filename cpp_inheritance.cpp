f61e16d8aee72da2d48f3442955ad1bd58e63c84
Yogabudi <yogabudiyulianto@gmail.com>
2021-10-13 09:04:32 +0700

    penambahan file cpp tentang inheritance

---
+++ cpp_inheritance.cpp
@@ -0,0 +1,46 @@
+class Vehicle {
+  public:
+    int kecepatan;
+    
+    Vehicle(int kecepatan) {
+      this->kecepatan = kecepatan;
+    }
+    
+    void jalankan() {
+      cout << "kendaraan berjalan...";
+    }
+    
+    void stop() {
+      cout << "kendaraan berhenti...";
+    }
+}
+
+class Truck : public Vehicle {
+  public:
+    int kapasitas;
+    char* warna;
+    
+    Truck(int kecepatan, int kapasitas, char* warna) : Vehicle(kecepatan) {
+      this->kapasitas = kapasitas;
+      this->warna = warna;
+    }
+    
+    void masukkanMuatan() {
+      cout << "memasukkan muatan...";
+    }
+}
+
+class Car : public Vehicle {
+  public:
+    int warna;
+    char* jenis;
+    
+    Car(char* jenis, int warna, int kecepatan) : Vehicle(kecepatan) {
+      this->jenis = jenis;
+      this->warna = warna;
+    }
+    
+    void nyalakanLampu() {
+      cout << "menyalakan lampu...";
+    }
+}
