1a140bcfa7bfd23858cca2f5e4888e44d20a4378
Yogabudi <yogabudiyulianto@gmail.com>
2021-10-18 11:31:18 +0700

    penambahan method untuk membuat angka random

--- main.cpp
+++ main.cpp
@@ -2,9 +2,46 @@
 
 using namespace std;
 
+void angkaRandomNonRepetisi(int hasil[], int panjangArray) {
+  srand(time(NULL));
+  hasil[0] = (rand() % panjangArray);
+
+  for(int i = 1; i < panjangArray; i++) {
+    hasil[i] = -1;
+  }
+
+  for(int i = 1; i < panjangArray; i++) {
+    bool valid = true;
+    int r = 0;
+    int angkaRandom = (rand() % panjangArray);
+
+    while(hasil[r] != -1) {
+      if(hasil[r] == angkaRandom) {
+        valid = false;
+        break;
+      }
+      r++;
+    }
+
+    if(valid) {
+      hasil[i] = angkaRandom;
+    }
+    else {
+      i--;
+    }
+  }
+}
+
 int main(int argc, char** argv) {
-	cout << "Hello world!";
-	cout << endl;
+	int banyaknya = 10;
+	int numbers[banyaknya];
+
+	angkaRandomNonRepetisi(numbers, banyaknya);
+
+	for(int i = 0; i < banyaknya; i++) {
+		cout << numbers[i] + 1 << " ";
+	}
+
 	cout << endl;
 	
 	return 0;
